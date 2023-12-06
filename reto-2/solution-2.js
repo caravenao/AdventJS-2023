89fc52fe3c0ffcf6bc3b31b303396c0ee886a81f
Cris Aravena <c.aravenaolea@gmail.com>
2023-12-06 20:12:11 -0300

    feat: solucion reto 2

---
+++ reto-2/solution-2.js
@@ -0,0 +1,23 @@
+function manufacture(gifts, materials) {
+    return gifts.filter(gift => {
+      return gift.split('').every(a => materials.includes(a));
+    })
+}
+
+const gifts = ['tren', 'oso', 'pelota']
+const materials = 'tronesa'
+
+manufacture(gifts, materials) // ["tren", "oso"]
+// 'tren' SÍ porque sus letras están en 'tronesa'
+// 'oso' SÍ porque sus letras están en 'tronesa'
+// 'pelota' NO porque sus letras NO están en 'tronesa'
+
+gifts = ['juego', 'puzzle']
+materials = 'jlepuz'
+
+manufacture(gifts, materials) // ["puzzle"]
+
+gifts = ['libro', 'ps5']
+materials = 'psli'
+
+manufacture(gifts, materials) // []
